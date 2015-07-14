69e0eac9b23002c82a846de8634f6e6389fc4351
Matthew O'Gorman <mog@rldn.net>
2015-07-13 22:08:36 -0400

    this is funky but should give me one more block

--- ci.sh
+++ ci.sh
@@ -9,5 +9,13 @@ cd ..
 cd salt_test
 xvfb-run --server-args="-screen 0 10x10x24" bash -c "gnuradio-companion salt_test.grc  & sleep 10; xdotool  key --clearmodifiers Print ; sleep 1; xdotool key --clearmodifiers Right BackSpace BackSpace BackSpace BackSpace Return; sleep 2; xdotool key --clearmodifiers Ctrl+q"
 mv salt_test.png ~/artifacts/mogs_grc_fun/build/
-
+cd ..
 echo it works?
+cd tutorial_test
+mkdir -p ~/.gnuradio
+echo "[grc]" > ~/.gnuradio/config.conf
+echo "local_blocks_path=../gr-tutorial/grc/" >> ~/.gnuradio/config.conf
+xvfb-run --server-args="-screen 0 10x10x24" bash -c "gnuradio-companion tutorial_test.grc  & sleep 10; xdotool  key --clearmodifiers Print ; sleep 1; xdotool key --clearmodifiers Right BackSpace BackSpace BackSpace BackSpace Return; sleep 2; xdotool key --clearmodifiers Ctrl+q"
+mv tutorial_test.png ~/artifacts/mogs_grc_fun/build/
+rm ~/.gnuradio/config.conf
+
