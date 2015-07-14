e491d9765d955d52d2e2c6079c765ab134ffa118
Matthew O'Gorman <mog@rldn.net>
2015-07-13 21:52:07 -0400

    it works but i need to add the blocks for it to be happy

--- ci.sh
+++ ci.sh
@@ -4,7 +4,7 @@ cd boats
 xvfb-run --server-args=" -screen 0 10x10x24" bash -c "gnuradio-companion boats.grc  & sleep 10; xdotool  key --clearmodifiers Print ; sleep 1; xdotool key --clearmodifiers Right BackSpace BackSpace BackSpace BackSpace Return; sleep 2; xdotool key --clearmodifiers Ctrl+q"
 mkdir -p ~/artifacts/mogs_grc_fun/build
 mv boats.png ~/artifacts/mogs_grc_fun/build/
-cd..
+cd ..
 
 cd salt_test
 xvfb-run --server-args="-screen 0 10x10x24" bash -c "gnuradio-companion salt_test.grc  & sleep 10; xdotool  key --clearmodifiers Print ; sleep 1; xdotool key --clearmodifiers Right BackSpace BackSpace BackSpace BackSpace Return; sleep 2; xdotool key --clearmodifiers Ctrl+q"
