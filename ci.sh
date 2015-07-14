73a6e2711b94e4c15fbd95bca6139f311a797d48
Matthew O'Gorman <mog@rldn.net>
2015-07-13 21:48:00 -0400

    updated script

--- ci.sh
+++ ci.sh
@@ -2,5 +2,11 @@
 ls
 cd boats
 xvfb-run ----server-args="10x10x24" bash -c "gnuradio-companion boats.grc  & sleep 10; xdotool  key --clearmodifiers Print ; sleep 1; xdotool key --clearmodifiers Right BackSpace BackSpace BackSpace BackSpace Return; sleep 2; xdotool key --clearmodifiers Ctrl+q"
-mkdir -p ~/artifacts/mog_grc_fun/build
-mv boats.png ~/artifacts/mog_grc_fun/build/
+mkdir -p ~/artifacts/mogs_grc_fun/build
+mv boats.png ~/artifacts/mogs_grc_fun/build/
+cd..
+
+cd salt_test
+xvfb-run ----server-args="10x10x24" bash -c "gnuradio-companion salt_test.grc  & sleep 10; xdotool  key --clearmodifiers Print ; sleep 1; xdotool key --clearmodifiers Right BackSpace BackSpace BackSpace BackSpace Return; sleep 2; xdotool key --clearmodifiers Ctrl+q"
+mv salt_test.png ~/artifacts/mogs_grc_fun/build/
+
