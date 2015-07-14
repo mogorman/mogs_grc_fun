3dbd0cd216ca2a8a23efaaf368f11393b77c2f01
Matthew O'Gorman <mog@rldn.net>
2015-07-13 21:44:16 -0400

    this should put a file in artifacts

--- ci.sh
+++ ci.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 ls
-
+cd boats
+xvfb-run ----server-args="10x10x24" bash -c "gnuradio-companion boats.grc  & sleep 10; xdotool  key --clearmodifiers Print ; sleep 1; xdotool key --clearmodifiers Right BackSpace BackSpace BackSpace BackSpace Return; sleep 2; xdotool key --clearmodifiers Ctrl+q"
+mkdir -p ~/artifacts/mog_grc_fun/build
+mv boats.png ~/artifacts/mog_grc_fun/build/
