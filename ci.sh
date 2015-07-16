55ca55011cbce38a19337e22a44853e6e2230873
Matthew O'Gorman <mog@rldn.net>
2015-07-16 12:57:58 -0400

    more flowgraphs

--- ci.sh
+++ ci.sh
@@ -18,4 +18,14 @@ echo "local_blocks_path=../gr-tutorial/grc/" >> ~/.gnuradio/config.conf
 xvfb-run --server-args="-screen 0 10x10x24" bash -c "gnuradio-companion tutorial_test.grc  & sleep 10; xdotool  key --clearmodifiers Print ; sleep 1; xdotool key --clearmodifiers Right BackSpace BackSpace BackSpace BackSpace Return; sleep 2; xdotool key --clearmodifiers Ctrl+q"
 mv tutorial_test.png ~/artifacts/mogs_grc_fun/build/
 rm ~/.gnuradio/config.conf
+cd ..
+
+cd fm_transmit
+mkdir -p ~/.gnuradio
+echo "[grc]" > ~/.gnuradio/config.conf
+echo "local_blocks_path=../gr-tutorial/grc/" >> ~/.gnuradio/config.conf
+xvfb-run --server-args="-screen 0 10x10x24" bash -c "gnuradio-companion fm_transmit.grc  & sleep 10; xdotool  key --clearmodifiers Print ; sleep 1; xdotool key --clearmodifiers Right BackSpace BackSpace BackSpace BackSpace Return; sleep 2; xdotool key --clearmodifiers Ctrl+q"
+mv fm_transmit.png ~/artifacts/mogs_grc_fun/build/
+rm ~/.gnuradio/config.conf
+cd ..
 
